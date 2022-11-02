6ad71c15ab972cb1a34223eced1429529b4f4e8d
DingWeikang <1374146671@qq.com>
2022-11-02 12:06:32 -0400

    Desision Tree and Random Forest

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, classification_report
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
+from sklearn import metrics
+import matplotlib.pyplot as plt
 import warnings
 
 warnings.filterwarnings("ignore")
@@ -46,6 +48,23 @@ EndTime_Testing = time.time()
 print("Testing time: ", EndTime_Testing - StartTime_Testing)
 print_report(y_test, y_test_DecisionTree_pred)
 
+y_pred1= DecisionTree_model.predict_proba(X_test)[:, 1]
+auc = metrics.roc_auc_score(y_test, y_pred1)
+
+false_positive_rate1, true_positive_rate1, thresolds1 = metrics.roc_curve(y_test, y_pred1)
+
+plt.figure(figsize=(10, 8), dpi=100)
+plt.axis('scaled')
+plt.xlim([0, 1])
+plt.ylim([0, 1])
+plt.title("Decision Tree - AUC & ROC Curve")
+plt.plot(false_positive_rate1, true_positive_rate1, 'g')
+plt.fill_between(false_positive_rate1, true_positive_rate1, facecolor='lightgreen', alpha=0.7)
+plt.text(0.95, 0.05, 'AUC = %0.4f' % auc, ha='right', fontsize=12, weight='bold', color='blue')
+plt.xlabel("False Positive Rate")
+plt.ylabel("True Positive Rate")
+plt.show()
+
 # Random Forest
 print("Random Forest")
 RandomForest_model = RandomForestClassifier()
@@ -58,3 +77,21 @@ y_test_RandomForest_pred = RandomForest_model.predict(X_test)
 EndTime_Testing = time.time()
 print("Testing time: ", EndTime_Testing - StartTime_Testing)
 print_report(y_test, y_test_RandomForest_pred)
+
+
+y_pred2 = RandomForest_model.predict_proba(X_test)[:, 1]
+auc = metrics.roc_auc_score(y_test, y_pred2)
+
+false_positive_rate2, true_positive_rate2, thresolds2 = metrics.roc_curve(y_test, y_pred2)
+
+plt.figure(figsize=(10, 8), dpi=100)
+plt.axis('scaled')
+plt.xlim([0, 1])
+plt.ylim([0, 1])
+plt.title("Random Forest - AUC & ROC Curve")
+plt.plot(false_positive_rate2, true_positive_rate2, 'g')
+plt.fill_between(false_positive_rate2, true_positive_rate2, facecolor='lightgreen', alpha=0.7)
+plt.text(0.95, 0.05, 'AUC = %0.4f' % auc, ha='right', fontsize=12, weight='bold', color='blue')
+plt.xlabel("False Positive Rate")
+plt.ylabel("True Positive Rate")
+plt.show()
